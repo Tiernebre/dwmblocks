385c6cb901d9243f0584873b2d41326dec91e6fd
Brendan Tierney <tiernebre@gmail.com>
2020-06-07 14:28:53 -0600

    change order

--- blocks.h
+++ blocks.h
@@ -1,13 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"",	"weather",	18000,	5},
 	{"",	"memory",	10,	14},
 	{"",	"cpu",		10,	18},
-	{"",	"weather",	18000,	5},
 	{"",	"volume",	0,	10},
+	{"",	"internet",	5,	4},
 	{"",	"clock",	60,	1},
-	{"",	"internet",	5,	4}
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim = '|';
+static char delim = ' ';
